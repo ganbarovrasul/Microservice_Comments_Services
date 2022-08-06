5877d71a9c196debc77bbae29e890d5a88a24240
ganbarovrasul <93209831+ganbarovrasul@users.noreply.github.com>
2022-08-06 17:00:36 +0400

    Add files via upload

--- routers.py
+++ routers.py
@@ -1,17 +1,21 @@
-from flask import jsonify , request
+import requests
+from flask import jsonify, request
 from repostories import get_comments, create_comment
 from app import app
+import time
 
 
-
-@app.route('/posts/<int:post_id>/comments',methods = ['GET',"POST"])
+@app.route('/posts/<int:post_id>/comments', methods = ['GET', "POST"])
 def comments(post_id):
     if request.method == 'POST':
         content = request.json['content']
-        new_comment = create_comment(content=content,blog_id=post_id)
+        new_comment = create_comment(content=content, blog_id=post_id)
+        post_data = {
+            'type': 'CommentCreated',
+            'data': new_comment
+        }
+        requests.post('http://127.0.0.1:5003/events', json=post_data)
         return jsonify(new_comment), 201
+    time.sleep(1)    
     comments = get_comments(blog_id=post_id)
-
     return jsonify(comments), 200
-
-    
